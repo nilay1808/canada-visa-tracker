ad57b2a19b2d7674cc2706eb0e6c5f88461a01e2
Nilay Sadavarte <nilay18sadavarte@gmail.com>
2023-12-07 17:39:33 -0600

    fix configs

--- packages/database/tsup.config.ts
+++ packages/database/tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig, type Options } from "tsup";
 
 export default defineConfig((options: Options) => ({
-  entryPoints: ["src/index.ts"],
+  entryPoints: ["src/index.ts", "src/migrate.ts"],
   clean: true,
   dts: true,
   format: ["cjs"],
