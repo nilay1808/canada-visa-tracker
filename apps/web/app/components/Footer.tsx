55bbd743c537af314c02aa865cbd142f572f6bf7
Nilay Sadavarte <nilay18sadavarte@gmail.com>
2023-12-18 22:10:02 -0600

    Make footer more subtle

--- apps/web/app/components/Footer.tsx
+++ apps/web/app/components/Footer.tsx
@@ -3,10 +3,10 @@ import { Link } from "@remix-run/react";
 export function Footer() {
   return (
     <footer className="container flex items-center justify-center mt-auto">
-      <div className="flex items-center text-sm font-light text-gray-600 dark:text-gray-400 mb-4 mt-8">
-        Made with ❤️ by&nbsp;
-        <Link className="font-medium" to="https://n18.dev">
-          Nilay
+      <div className="flex items-center text-xs font-light text-gray-400 dark:text-gray-600 mb-4 mt-8">
+        Made by&nbsp;
+        <Link className="font-normal" to="https://n18.dev">
+          Nilay Sadavarte
         </Link>
       </div>
     </footer>
