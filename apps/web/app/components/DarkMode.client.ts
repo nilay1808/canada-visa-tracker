89372f3e2928a65e91d107ddb402615fe9a6a05c
Nilay Sadavarte <nilay18sadavarte@gmail.com>
2023-12-21 17:09:19 -0600

    Check color scheme preference

--- apps/web/app/components/DarkMode.client.ts
+++ apps/web/app/components/DarkMode.client.ts
@@ -1,6 +1,14 @@
 export function getDarkModeValueFromLocalStorage() {
   const localValue = window.localStorage.getItem("darkMode");
 
+  if (localValue === null) {
+    const prefersDark =
+      window.matchMedia &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+    return prefersDark;
+  }
+
   return localValue === "true";
 }
 
